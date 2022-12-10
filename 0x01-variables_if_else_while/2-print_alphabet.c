ff112598c0b43758f9104945d0b07eb5d126b936
kelvinmburu <mburuhkelvin@gmail.com>
2022-12-09 16:58:59 -0800

    added file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+	
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
